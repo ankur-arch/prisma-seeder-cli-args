802ffd07d3670fec245039917a52b88da69d9f6a
Ankur Datta <dev.datta.ankur@gmail.com>
2023-07-17 13:10:21 +0600

    feat: light run on accelerate test

--- script.ts
+++ script.ts
@@ -31,86 +31,31 @@ registerInstrumentations({
 });
 
 import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 const db = new PrismaClient();
-
-const createMany = (n: number) => {
-  return new Array(n)
-    .fill({
-      email: " ",
-      name: " ",
-    })
-    .map((item) => ({
-      name: (Math.random() * 100).toString(),
-      email: (Math.random() * 100).toString(),
-    }));
-};
+const acc = new PrismaClient().$extends(withAccelerate());
 
 const main = async () => {
-  const creates2 = createMany(2);
-  const creates20 = createMany(20);
-  const creates100 = createMany(100);
-  const creates1000 = createMany(1000);
-  const icreates2 = createMany(2);
-  const icreates20 = createMany(20);
-  const icreates100 = createMany(100);
-  const icreates1000 = createMany(1000);
+  for (let index = 0; index < 20; index++) {
+    console.time('regular')
+    await db.notes.findFirst();
+    console.timeEnd('regular')
 
-  console.time("Testing 2");
-  await db.user.createMany({
-    data: creates2,
-  });
-  console.timeEnd("Testing 2");
+    console.time('QE')
+    await acc.notes.findFirst();
+    console.timeEnd('QE')
 
-  console.time("Testing 20");
-  await db.user.createMany({
-    data: creates20,
-  });
-  console.timeEnd("Testing 20");
-
-  console.time("Testing 100");
-  await db.user.createMany({
-    data: creates100,
-  });
-  console.timeEnd("Testing 100");
-
-  console.time("Testing 1000");
-  await db.user.createMany({
-    data: creates1000,
-  });
-  console.timeEnd("Testing 1000");
-
-  console.time("Itx: Testing 2");
-  await db.$transaction(async (p) => {
-    await p.user.createMany({
-      data: icreates2,
+    console.time('Cache')
+    await acc.notes.findFirst({
+      cacheStrategy: {
+        ttl: 100,
+      },
     });
-  });
-  console.timeEnd("Itx: Testing 2");
+    console.timeEnd('Cache')
 
-  console.time("Itx: Testing 20");
-  await db.$transaction(async (p) => {
-    await p.user.createMany({
-      data: icreates20,
-    });
-  });
-  console.timeEnd("Itx: Testing 20");
-
-  console.time("Itx: Testing 100");
-  await db.$transaction(async (p) => {
-    await p.user.createMany({
-      data: icreates100,
-    });
-  });
-  console.timeEnd("Itx: Testing 100");
-
-  console.time("Itx: Testing 1000");
-  await db.$transaction(async (p) => {
-    await p.user.createMany({
-      data: icreates1000,
-    });
-  });
-  console.timeEnd("Itx: Testing 1000");
+    console.log('----------------------------------')
+  }
 };
 
 main().catch(console.log);
